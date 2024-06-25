4beccb80537bdfff863b96cb0cec7430e61997f8
Karl Belousov <belousovkarl@gmail.com>
2024-06-25 15:11:53 +0600

    сделал компоненты страниц и компонент маленькой карточки фильма

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+
+type AppProps = {
+  filmsCount: number;
+  film: {
+    title: string;
+    genre: string;
+    year: number;
+  };
+}
+
+function App({filmsCount, film}: AppProps): JSX.Element {
+  return (
+    <MainPage filmsCount={filmsCount} film={film} />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FILMS_COUNT = 20;
+
+const film = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App filmsCount={FILMS_COUNT} film={film} />
   </React.StrictMode>,
 );
