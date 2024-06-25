d8ad313f976325d1d740904575d0114f8904d74c
Karl Belousov <belousovkarl@gmail.com>
2024-06-25 15:49:08 +0600

    исправление ошибок по линтеру

--- project/src/pages/add-review-page/add-review-page.tsx
+++ project/src/pages/add-review-page/add-review-page.tsx
@@ -167,7 +167,7 @@ function AddReviewPage(): JSX.Element {
               name="review-text"
               id="review-text"
               placeholder="Review text"
-              defaultValue={""}
+              defaultValue={''}
             />
             <div className="add-review__submit">
               <button className="add-review__btn" type="submit">
