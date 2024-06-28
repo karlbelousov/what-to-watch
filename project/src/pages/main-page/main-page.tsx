92f86f7d7fcb6f9b9eff86f1467a7586d9364794
Karl Belousov <belousovkarl@gmail.com>
2024-06-28 13:54:47 +0600

    сделал компонент логотипа

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import SmallFilmCard from '../../components/small-film-card/small-film-card';
+import Logo from '../../components/logo/logo';
 
 type MainPageProps = {
   filmsCount: number;
@@ -21,13 +22,7 @@ function MainPage({filmsCount, film}: MainPageProps): JSX.Element {
         </div>
         <h1 className="visually-hidden">WTW</h1>
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo />
           <ul className="user-block">
             <li className="user-block__item">
               <div className="user-block__avatar">
