9caedd2943173fe31292715cfd903a93b737d84a
Karl Belousov <belousovkarl@gmail.com>
2024-07-01 17:02:13 +0600

    в компоненте small-film-card отрисовываю временные данные о фильме

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,17 +1,19 @@
-function SmallFilmCard() {
+import { Film } from '../../types/film';
+
+function SmallFilmCard({name, previewImage}: Film) {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
         <img
-          src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-          alt="Fantastic Beasts: The Crimes of Grindelwald"
+          src={previewImage}
+          alt={name}
           width={280}
           height={175}
         />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">
-          Fantastic Beasts: The Crimes of Grindelwald
+          {name}
         </a>
       </h3>
     </article>
