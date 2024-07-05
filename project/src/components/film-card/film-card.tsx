9fffaf7b946ba9fc7275cc15af01f47b412c5573
Karl Belousov <belousovkarl@gmail.com>
2024-07-05 14:19:59 +0600

    подключил компонент видеоплеера в карточку фильма

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,37 +1,48 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Film } from '../../types/film';
 import { AppRoute } from '../../const';
+import VideoPlayer from '../video-player/video-player';
 
-type FilmCardProps = Film & {
-  onMouseMove: (id: number) => void;
-  onMouseLeave: () => void;
-}
 
 function FilmCard({
   name,
   previewImage,
   id,
-  onMouseMove,
-  onMouseLeave
-}: FilmCardProps) {
-  const handleMouseMove = () => {
-    onMouseMove(id);
+  previewVideoLink,
+}: Film) {
+  const [activeFilm, setActiveFilm] = useState<number | null>(null);
+
+  const handleMouseMove = (filmId: number) => {
+    setActiveFilm(filmId);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveFilm(null);
   };
 
   return (
     <article
       className="small-film-card catalog__films-card"
-      onMouseMove={handleMouseMove}
-      onMouseLeave={onMouseLeave}
+      onMouseMove={() => handleMouseMove(id)}
+      onMouseLeave={handleMouseLeave}
     >
-      <div className="small-film-card__image">
-        <img
-          src={previewImage}
-          alt={name}
-          width={280}
-          height={175}
+      {(activeFilm !== id) ? (
+        <div className="small-film-card__image">
+          <img
+            src={previewImage}
+            alt={name}
+            width={280}
+            height={175}
+          />
+        </div>
+      ) : (
+        <VideoPlayer
+          previewImage={previewImage}
+          previewVideoLink={previewVideoLink}
+          activeFilm={activeFilm}
         />
-      </div>
+      )}
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`${AppRoute.Film}/${id}`}>
           {name}
