9f0c53ad53c030ad9192856d5fc12e28da9f9ac9
Karl Belousov <belousovkarl@gmail.com>
2024-07-05 14:18:54 +0600

    убрал состояние активного фильма в компонент карточки фильма

--- project/src/components/film-card-list/fillm-card-list.tsx
+++ project/src/components/film-card-list/fillm-card-list.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import FilmCard from '../../components/film-card/film-card';
 import { Film } from '../../types/film';
 
@@ -7,25 +6,12 @@ type FilmCardListProps = {
 }
 
 function FilmCardList({films}: FilmCardListProps) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [activeFilm, setActiveFilm] = useState<number | null>(null);
-
-  const handleMouseMove = (id:number) => {
-    setActiveFilm(id);
-  };
-
-  const handleMouseLeave = () => {
-    setActiveFilm(null);
-  };
-
   return (
     <div className="catalog__films-list">
       {films.map((film) => (
         <FilmCard
           key={film.id}
           {...film}
-          onMouseMove={handleMouseMove}
-          onMouseLeave={handleMouseLeave}
         />
       ))}
     </div>
